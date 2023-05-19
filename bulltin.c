640621de36db8a131a7c7b1afe9cc3696c9949a4
abutalep <ahmedabutalep10@gmail.com>
2023-05-19 10:41:54 -0700

    o

--- bulltin.c
+++ bulltin.c
@@ -1,135 +1,65 @@
 #include "shell.h"
 
-/**
- * exit_bul - Exit Statue Shell
- * @cmd: Parsed Command
- * @input: User Input
- * @argv:Program Name
- * @c:Excute Count
- * Return: Void (Exit Statue)
- */
-void  exit_bul(char **cmd, char *input, char **argv, int c)
+int _myexit(info_t *info)
 {
-	int statue;
-
-	if (cmd[1] == NULL)
-	{
-		free(input);
-		free(cmd);
-		exit(EXIT_SUCCESS);
-	}
-
-	statue = atoi(cmd[1]);
-
-	free(input);
-	free(cmd);
-
-	exit(statue);
-}
-
-
-/**
- * change_dir - Change Dirctorie
- * @cmd: Parsed Command
- * @er: Statue Last Command Excuted
- * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
- */
-int change_dir(char **cmd, __attribute__((unused))int er)
-{
-	int value = -1;
-	char cwd[PATH_MAX];
-
-	if (cmd[1] == NULL)
-		value = chdir(getenv("HOME"));
-	else if (strcmp(cmd[1], "-") == 0)
-	{
-		value = chdir(getenv("OLDPWD"));
-	}
-	else
-	{
-		value = chdir(cmd[1]);
-	}
-
-	if (value == -1)
-	{
-		perror("hsh");
-		return (-1);
-	}
-	else if (value != -1)
-	{
-		getcwd(cwd, sizeof(cwd));
-		setenv("OLDPWD", getenv("PWD"), 1);
-		setenv("PWD", cwd, 1);
-	}
-	return (0);
-}
-/**
- * dis_env - Display Enviroment Variable
- * @cmd:Parsed Command
- * @er:Statue of Last command Excuted
- * Return:Always 0
- */
-int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
-{
-size_t i;
-	int len;
-
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		len = strlen(environ[i]);
-		write(1, environ[i], len);
-		write(STDOUT_FILENO, "\n", 1);
-	}
-	return (0);
-}
-/**
- * display_help - Displaying Help For Builtin
- * @cmd:Parsed Command
- * @er: Statue Of Last Command Excuted
- * Return: 0 Succes -1 Fail
- */
-int display_help(char **cmd, __attribute__((unused))int er)
-{
-	int fd, fw, rd = 1;
-	char c;
-
-	fd = open(cmd[1], O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error");
-		return (0);
-	}
-	while (rd > 0)
-	{
-		rd = read(fd, &c, 1);
-		fw = write(STDOUT_FILENO, &c, rd);
-		if (fw < 0)
-		{
-			return (-1);
+	if (info->argv[1]) {
+		int exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1) {
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return 1;
 		}
+		info->err_num = _erratoi(info->argv[1]);
+		return -2;
 	}
-	_putchar('\n');
-	return (0);
+	info->err_num = -1;
+	return -2;
 }
-/**
- * echo_bul - Excute Echo Cases
- * @st:Statue Of Last Command Excuted
- * @cmd: Parsed Command
- * Return: Always 0 Or Excute Normal Echo
- */
-int echo_bul(char **cmd, int st)
-{
-	char *path;
-	unsigned int  pid = getppid();
 
-	if (strcmp(cmd[1], "<span class="math-inline">?"\) \=\= 0\)
-\{
-printf\("%d\\n", st\);
-\}
-else if \(strcmp\(cmd\[1\], "</span>$") == 0)
-	{
-		printf("%d\n", pid);
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+
+	if (!info->argv[1]) {
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	} else if (_strcmp(info->argv[1], "-") == 0) {
+		if (!_getenv(info, "OLDPWD=")) {
+			_puts(s);
+			_putchar('\n');
+			return 1;
+		}
+		_puts(_getenv(info, "OLDPWD="));
+		_putchar('\n');
+		chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	} else {
+		chdir_ret = chdir(info->argv[1]);
 	}
-	else if (strcmp(cmd[1], "$PATH") == 0)
-	{
-		path = getenv("PATH");
+
+	if (chdir_ret == -1) {
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]);
+		_eputchar('\n');
+	} else {
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+
+	return 0;
+}
+
+int _myhelp(info_t *info)
+{
+	_puts("help call works. Function not yet implemented \n");
+	return 0;
+}
